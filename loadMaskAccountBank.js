e17a06b38bc95827376c98fb5e108ff1e8cf80e0
Jonnie Henrique <jonniehenry@gmail.com>
2016-09-06 14:33:31 -0300

    Function de mascára de contas bancárias

---
+++ loadMaskAccountBank.js
@@ -0,0 +1,13 @@
+function loadMaskContaBancaria(contaBancaria) {
+	
+	var valSemDigito = contaBancaria.val().replace(/[^0-9]/g, '');
+        
+	if (contaBancaria.val().length === 0) {
+		contaBancaria.val('');
+		return false;
+	}
+            
+	if (contaBancaria.val().length >= 1) {
+		contaBancaria.val(valSemDigito.slice(0, valSemDigito.length - 1) + '-' + valSemDigito.slice(valSemDigito.length - 1, valSemDigito.length));
+	}
+};
